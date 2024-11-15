090c701fe5b8ede73747af06b9b947687d5b6906
byui-dev <okdiphala@hotmail.com>
2024-11-15 14:02:48 +0200

    new commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,17 @@
 const input = document.querySelector('#favchap');
+button.addEventListener('click', function() {
+});
+if (input.value.trim( !== '') { ...}
 const button = document.querySelector('button');
 const list = document.querySelector('________'); // The HTML element that is the unordered list needs to be filled in
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
+deleteButton.addEventListener('click', function () {
+  list.removeChild(li);
+  input.focus();
+});
+input.value = '';
+input.focus();      
 li.textContent = input.value;
 deleteButton.textContent = 'X';
 li.append(deleteButton);
